44dca18fcbcfed4c186c4567165124cc14436360
shubham pawar <sp8019036@gmail.com>
2026-04-02 01:42:21 +0530

    Fix typo in comment for truncateSentence function

--- Easy/Strings/1816.Truncate-Sentence.c
+++ Easy/Strings/1816.Truncate-Sentence.c
@@ -11,7 +11,7 @@ Approach:Simulation/Two Pointers
 char* truncateSentence(char* s, int k) {
     int i=0,count=0;   
     while(s[i]!='\0'){  
-        if(s[i]==' '){    //in this question there is no leading space it mean if one spce fount mean 1 word complete
+        if(s[i]==' '){    //in this question there is no leading space it mean if one space found mean 1 word complete
             count++;    
         }
         if(count==k){
