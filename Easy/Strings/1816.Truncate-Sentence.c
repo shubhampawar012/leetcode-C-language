39b806f9e233ed0b741e3c8a8e4ae4f96bcf154e
shubhampawar012 <sp8019036@gmail.com>
2026-04-01 19:43:38 +0530

    1816.Truncate-Sentence

---
+++ Easy/Strings/1816.Truncate-Sentence.c
@@ -0,0 +1,24 @@
+/*
+link:https://leetcode.com/problems/truncate-sentence/submissions/1965889303
+
+Runtime:0ms
+Beats:100.00%
+Approach:Simulation/Two Pointers
+
+
+*/
+
+char* truncateSentence(char* s, int k) {
+    int i=0,count=0;   
+    while(s[i]!='\0'){  
+        if(s[i]==' '){    //in this question there is no leading space it mean if one spce fount mean 1 word complete
+            count++;    
+        }
+        if(count==k){
+            s[i]='\0';   //if count==k   then we cut the string  
+            break;
+        }
+        i++;
+    } 
+    return s;
+}
